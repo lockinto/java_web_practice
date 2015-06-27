38f6007d9f2ca49b5a21bc69480ba93d38a927fb
924354349 <924354349@qq.com>
2015-06-27 22:09:16 +0800

    add something

---
+++ ServletPractice/src/com/into/CServlet.java
@@ -0,0 +1,38 @@
+package com.into;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CServlet extends HttpServlet {
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		ServletContext application = this.getServletContext();
+		Integer count = (Integer) application.getAttribute("count");
+		if (count == null)
+			application.setAttribute("count",1);
+		else
+			application.setAttribute("count",count+1);
+		
+		PrintWriter pw = response.getWriter();
+		pw.print("<h1>" + "这是您第" + count + "次访问本站<h1>");
+	}
+
+}
---
+++ ServletPractice/src/com/into/DServlet.java
@@ -0,0 +1,38 @@
+package com.into;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+
+public class DServlet extends HttpServlet {
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		ClassLoader cl = this.getClass().getClassLoader();
+		InputStream input = cl.getResourceAsStream("/../../index.jsp");
+		
+		String str = IOUtils.toString(input);
+		PrintWriter pw = response.getWriter();
+		System.out.println(str);
+		pw.print("<h1>" + str + "<h1>");
+	}
+
+}

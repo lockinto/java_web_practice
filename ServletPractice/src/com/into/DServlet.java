f9488305b69129b81a485eb9811ffaa7bed5b73d
924354349 <924354349@qq.com>
2015-06-28 16:23:35 +0800

    做了一些练习

--- ServletPractice/src/com/into/DServlet.java
+++ ServletPractice/src/com/into/DServlet.java
@@ -13,26 +13,20 @@ import org.apache.commons.io.IOUtils;
 
 public class DServlet extends HttpServlet {
 
-	/**
-	 * The doGet method of the servlet. <br>
-	 *
-	 * This method is called when a form has its tag value method equals to get.
-	 * 
-	 * @param request the request send by the client to the server
-	 * @param response the response send by the server to the client
-	 * @throws ServletException if an error occurred
-	 * @throws IOException if an error occurred
-	 */
+	    
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
+		
 		ClassLoader cl = this.getClass().getClassLoader();
 		InputStream input = cl.getResourceAsStream("/../../index.jsp");
-		
+
 		String str = IOUtils.toString(input);
 		PrintWriter pw = response.getWriter();
 		System.out.println(str);
 		pw.print("<h1>" + str + "<h1>");
 	}
+	@Override
+	
 
 }
